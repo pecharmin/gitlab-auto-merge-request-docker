da53813f4739564fe6a675c6b1cb3c23ec5ecf3b
Armin Pech <a.pech@babiel.com>
2019-12-01 19:49:48 +0100

    fix(deps): fix jq command

--- merge-request.sh
+++ merge-request.sh
@@ -83,7 +83,7 @@ do
 
 	# Require a list of all the merge request and take a look if there is already
 	# one with the same source branch
-	OPEN_MR="`curl --silent "${HOST}${CI_PROJECT_ID}/merge_requests?state=opened" --header "PRIVATE-TOKEN: ${GITLAB_PRIVATE_TOKEN}" | jq ".[] | select(.\"target_branch\"==\"${branch}\") | select(.\"source_branch\"==\"${CI_COMMIT_REF_NAME}\")"`"
+	OPEN_MR=`curl --silent "${HOST}${CI_PROJECT_ID}/merge_requests?state=opened" --header "PRIVATE-TOKEN: ${GITLAB_PRIVATE_TOKEN}" | jq ".[] | select(.\"target_branch\"==\"${branch}\") | select(.\"source_branch\"==\"${CI_COMMIT_REF_NAME}\")"`
 
 	# No MR found, let's create a new one
 	if [ -z "${OPEN_MR}" ]
